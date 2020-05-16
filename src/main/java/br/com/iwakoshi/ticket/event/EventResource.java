b59263396302f0f01719e7865546d96d9ff45c4b
Fabio Iwakoshi <fabio.iwakoshi@gmail.com>
2020-05-15 22:41:46 -0300

    deprecate event v1 api

--- src/main/java/br/com/iwakoshi/ticket/event/EventResource.java
+++ src/main/java/br/com/iwakoshi/ticket/event/EventResource.java
@@ -1,19 +1,19 @@
 package br.com.iwakoshi.ticket.event;
 
+import static br.com.iwakoshi.ticket.config.auth.Roles.ADMIN;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
 
-import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.annotation.JsonView;
 
 import br.com.iwakoshi.ticket.config.json.Views;
 import br.com.iwakoshi.ticket.config.json.Views.ComingSoon;
-import br.com.iwakoshi.ticket.config.rest.CachControl;
 import br.com.iwakoshi.ticket.config.rest.PageParam;
 import lombok.extern.java.Log;
 
@@ -36,41 +35,35 @@ import lombok.extern.java.Log;
 @Consumes(MediaType.APPLICATION_JSON)
 public class EventResource {
 
-	@Inject
-	private EventService eventService;
-
-	@Inject
-	@CachControl(maxAge = 20)
-	private CacheControl cacheControl;
-
 	/**
 	 * @param Event id
 	 * @return a specific Event
+	 * @deprecated a new version is available
+	 * @throws URISyntaxException
 	 */
 	@GET
 	@Path("{id:\\d+}")
 	@JsonView(Views.Event.class)
-	public Response findById(@PathParam("id") Long id) {
-		log.log(Level.FINE, () -> "fetching event by id: " + id);
-		Event event = eventService.find(id);
-		return Response.ok(eventService.find(id)).cacheControl(cacheControl)
-				.tag(Integer.toString(event.getId().hashCode())).build();
+	@Deprecated(since = "2.0", forRemoval = true)
+	public Response findById(@PathParam("id") Long id) throws URISyntaxException {
+		log.log(Level.FINE, () -> "unavailable fetching event by id: " + id);
+		return Response.seeOther(new URI("events/v2/" + id)).build();
 	}
 
 	/**
 	 * @param page
 	 * @param keyword
-	 * @return Coming Soon Movies
-	 * @deprecated The queryParam was changed
+	 * @return coming Soon Movies
+	 * @deprecated the queryParam was changed
 	 * @throws URISyntaxException
 	 */
 	@GET
 	@Path("coming-soon")
-	@PermitAll
+	@RolesAllowed({ ADMIN })
 	@JsonView(ComingSoon.class)
 	@Deprecated(since = "2.0", forRemoval = true)
 	public Response comingSoon(@BeanParam PageParam page) throws URISyntaxException {
-		log.log(Level.FINE, () -> "fetching all events");
+		log.log(Level.FINE, () -> "unavailable fetching all events");
 		return Response
 				.seeOther(new URI("events/v2/coming-soon?offset=" + page.getOffset() + "&limit=" + page.getLimit()))
 				.build();
